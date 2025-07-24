bf2032fe696207437400dd6af2103c6759c33960
Maks1122n <gridasovmaks4@gmail.com>
2025-07-24 16:12:59 +0300

    🎯 OrbitHub -> Single Page App: брали навигацию, остался только полнофункциональный KomboNew на главной странице

--- backend/src/controllers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './authController';
-export * from './accountController';
-export * from './dashboardController'; 
--- backend/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export { AdsPowerService } from './AdsPowerService';
-export { InstagramService } from './InstagramService';
-export { DropboxService } from './DropboxService';
-export { AutomationService } from './AutomationService'; 
